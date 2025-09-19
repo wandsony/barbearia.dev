3b7824dfb03afc1459a6a7dd79e2371749334ee1
Wandson Gomes <wandsony@gmail.com>
2025-09-19 14:45:05 -0300

    Add files via upload

--- api/horarios.js
+++ api/horarios.js
@@ -2,6 +2,8 @@ const db = require('./db');
 
 module.exports = (req, res) => {
   const { data } = req.query;
+  if (!data) return res.status(400).json({ erro: "Data não informada" });
+
   const horarios = [
     "09:00", "09:40", "10:20", "11:00", "11:40",
     "13:00", "13:40", "14:20", "15:00", "15:40",
@@ -9,9 +11,11 @@ module.exports = (req, res) => {
   ];
 
   db.all(`SELECT hora FROM agendamentos WHERE data = ?`, [data], (err, rows) => {
-    if (err) return res.status(500).send("Erro no banco");
+    if (err) return res.status(500).json({ erro: "Erro no banco" });
+
     const ocupados = rows.map(r => r.hora);
     const disponiveis = horarios.filter(h => !ocupados.includes(h));
     res.json(disponiveis);
   });
 };
+
